4b866d923f3f332c89a37c7a7c9886722999f131
Leanderson Costa <exmoser@gmail.com>
2026-08-07 07:40:28 +0100

    balance

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -3,7 +3,7 @@ class Enemy {
     this.scene = scene;
     this.speed = speed;
     this.sprite = scene.add.sprite(x, y, "enemy1");
-    this.sprite.setScale(0.9);
+    this.sprite.setScale(0.75);
 
     scene.physics.add.existing(this.sprite);
     this.animationTimer = scene.time.addEvent({
